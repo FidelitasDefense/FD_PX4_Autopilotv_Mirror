Airial-UAS/PX4-Autopilot

MCP9808: Create driver for MCP9808 temperature sensor

src/modules/logger/logged_topics.cpp
@@ -118,6 +118,7 @@ void LoggedTopics::add_default_topics()
 	add_topic("sensor_combined");
 	add_optional_topic("sensor_correction");
 	add_optional_topic("sensor_gyro_fft", 50);
+	add_optional_topic("sensor_mcp9808", 10);
 	add_topic("sensor_selection");
 	add_topic("sensors_status_imu", 200);
 	add_optional_topic("spoilers_setpoint", 1000);

FidelitasDefense/FD_PX4_Autopilotv_Mirror

src/modules/logger/logged_topics.cpp
@@ -120,6 +120,7 @@ void LoggedTopics::add_default_topics()
 	add_optional_topic("sensor_gyro_fft", 50);
 	add_topic("sensor_selection");
 	add_topic("sensors_status_imu", 200);
+	add_optional_topic("sensor_temp", 10);
 	add_optional_topic("spoilers_setpoint", 1000);
 	add_topic("system_power", 500);
 	add_optional_topic("takeoff_status", 1000);